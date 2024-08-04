981b1b735b6f04aaa010bd64d4846880c60678b4
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-04 20:16:35 +0530

    first commit , initial setup to work with shadcn ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='bg-white'>
+      <Button>Hello World</Button>
+    </div>
+  )
+}
+
+export default App
