4d8ec7f7904485dfe603076c9851c3ad3d916642
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-06 23:10:31 +0530

    comments

--- backend/controllers/order.Controller.js
+++ backend/controllers/order.Controller.js
@@ -1,55 +1,64 @@
+// Import necessary modules and middleware
 const catchAsyncError = require('../middlewares/catchAsyncError.Middleware')
 const Order = require('../models/order.Model')
 const ErrorHandler = require('../utils/errorHandlerClass')
 
-//Endpoint: api/v1/order/new
+// Endpoint: api/v1/order/new
+// Create a new order
 exports.newOrder = catchAsyncError(async (req, res, next) => {
-	const {
-		orderItems,
-		shippingInfo,
-		itemsPrice,
-		taxPrice,
-		shippingPrice,
-		totalPrice,
-		paymentInfo,
-	} = req.body
+    // Destructure order details from the request body
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+    } = req.body
 
-	const order = await Order.create({
-		orderItems,
-		shippingInfo,
-		itemsPrice,
-		taxPrice,
-		shippingPrice,
-		totalPrice,
-		paymentInfo,
-		paidAt: Date.now(),
-		user: req.user.id,
-	})
+    // Create a new order in the database
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(), // Set the paid date to the current date and time
+        user: req.user.id, // Associate the order with the logged-in user
+    })
 
-	res.status(200).json({
-		success: true,
-		order,
-	})
+    // Send a success response with the created order
+    res.status(200).json({
+        success: true,
+        order,
+    })
 })
 
-//Endpoint: api/v1/order/:id
-//Get single order
+// Endpoint: api/v1/order/:id
+// Get a single order by ID
 exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
-	const order = await Order.findById(req.params.id).populate(
-		'user',
-		'name email'
-	)
-	if (!order) {
-		return next(
-			new ErrorHandler(
-				`Order not found with this id: ${req.params.id}`,
-				404
-			)
-		)
-	}
+    // Find the order by ID and populate the user details (name and email)
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
 
-	res.status(200).json({
-		success: true,
-		order,
-	})
+    // If the order is not found, return an error
+    if (!order) {
+        return next(
+            new ErrorHandler(
+                `Order not found with this id: ${req.params.id}`,
+                404
+            )
+        )
+    }
+
+    // Send a success response with the order details
+    res.status(200).json({
+        success: true,
+        order,
+    })
 })
