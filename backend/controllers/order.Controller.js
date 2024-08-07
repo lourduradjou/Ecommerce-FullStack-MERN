e17507cc8f41db2e9bb9991644904e4cbe98ec93
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-07 13:19:32 +0530

    Add GetMyOrders api for user

--- backend/controllers/order.Controller.js
+++ backend/controllers/order.Controller.js
@@ -6,59 +6,82 @@ const ErrorHandler = require('../utils/errorHandlerClass')
 // Endpoint: api/v1/order/new
 // Create a new order
 exports.newOrder = catchAsyncError(async (req, res, next) => {
-    // Destructure order details from the request body
-    const {
-        orderItems,
-        shippingInfo,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paymentInfo,
-    } = req.body
+	// Destructure order details from the request body
+	const {
+		orderItems,
+		shippingInfo,
+		itemsPrice,
+		taxPrice,
+		shippingPrice,
+		totalPrice,
+		paymentInfo,
+	} = req.body
 
-    // Create a new order in the database
-    const order = await Order.create({
-        orderItems,
-        shippingInfo,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paymentInfo,
-        paidAt: Date.now(), // Set the paid date to the current date and time
-        user: req.user.id, // Associate the order with the logged-in user
-    })
+	// Create a new order in the database
+	const order = await Order.create({
+		orderItems,
+		shippingInfo,
+		itemsPrice,
+		taxPrice,
+		shippingPrice,
+		totalPrice,
+		paymentInfo,
+		paidAt: Date.now(), // Set the paid date to the current date and time
+		user: req.user.id, // Associate the order with the logged-in user
+	})
 
-    // Send a success response with the created order
-    res.status(200).json({
-        success: true,
-        order,
-    })
+	// Send a success response with the created order
+	res.status(200).json({
+		success: true,
+		order,
+	})
 })
 
 // Endpoint: api/v1/order/:id
 // Get a single order by ID
 exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
-    // Find the order by ID and populate the user details (name and email)
-    const order = await Order.findById(req.params.id).populate(
-        'user',
-        'name email'
-    )
+	// Find the order by ID and populate the user details (name and email)
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email'
+	)
 
-    // If the order is not found, return an error
-    if (!order) {
-        return next(
-            new ErrorHandler(
-                `Order not found with this id: ${req.params.id}`,
-                404
-            )
-        )
-    }
+	// If the order is not found, return an error
+	if (!order) {
+		return next(
+			new ErrorHandler(
+				`Order not found with this id: ${req.params.id}`,
+				404
+			)
+		)
+	}
 
-    // Send a success response with the order details
-    res.status(200).json({
-        success: true,
-        order,
-    })
+	// Send a success response with the order details
+	res.status(200).json({
+		success: true,
+		order,
+	})
+})
+
+//Get Loggedin Users Orders
+//Endpoint : /api/v1/myorders
+exports.getMyOrders = catchAsyncError(async (req, res, next) => {
+	// Find the order by ID and populate the user details (name and email)
+	const order = await Order.find({ user: req.user.id })
+
+	// If the order is not found, return an error
+	if (!order) {
+		return next(
+			new ErrorHandler(
+				`No order is there!`,
+				404
+			)
+		)
+	}
+
+	// Send a success response with the order details
+	res.status(200).json({
+		success: true,
+		order,
+	})
 })
--- backend/routes/order.Routes.js
+++ backend/routes/order.Routes.js
@@ -1,5 +1,9 @@
 const express = require('express')
-const { newOrder, getSingleOrder } = require('../controllers/order.Controller')
+const {
+	newOrder,
+	getSingleOrder,
+	getMyOrders,
+} = require('../controllers/order.Controller')
 const {
 	isAuthenticatedUser,
 } = require('../middlewares/authenticate.Middleware')
@@ -7,5 +11,6 @@ const router = express.Router()
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder)
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder)
+router.route('/myorders').get(isAuthenticatedUser, getMyOrders)
 
 module.exports = router
