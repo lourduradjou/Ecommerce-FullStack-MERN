eb78be1532496bf631f7205638a4097280ad4164
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-08 16:02:12 +0530

    Navbar pop up added profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Body from "./components/Body"
 const App = () => {
   return (
-    <div className='bg-slate-50 h-screen w-full'>
+    <div className='bg-slate-100 h-screen w-full'>
       <Navbar />
+      <Body />
       <Footer />
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,49 @@
 import React from 'react'
+import { Mail } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { DropdownMenuDemo } from './ui/dropDownDemo'
 
 const Navbar = () => {
 	return (
 		<div>
-			<nav class='bg-walmartBlue px-4 flex md:flex-row items-center justify-between select-none'>
-				<div class='flex items-center w-full'>
+			<nav className='bg-walmartBlue px-4 flex md:flex-row items-center justify-between select-none'>
+				<div className='flex items-center w-full'>
 					<img
-						class='w-16 rounded-md'
+						className='w-16 rounded-md'
 						src='./images/walmartLogo.png'
+						alt='Walmart Logo'
 					/>
-					<div class='flex-1 mx-4'>
-						<div class='relative w-1/2 md:w-3/4 mx-auto'>
+					<p className='text-medium text-xl p-2'>
+						Walmart -{' '}
+						<span className='text-yellow-500'>InvenOptix</span>
+					</p>
+					<div className='flex-1 mx-4'>
+						<div className='relative w-1/2 md:w-3/4 mx-auto'>
 							<input
 								type='text'
-								class='h-10 p-4 w-full border border-gray-300 outline-none rounded-full'
+								className='h-10 p-4 w-full border border-gray-300 outline-none rounded-full'
 								placeholder='Enter Product Name ...'
 							/>
 							<button
 								id='search_btn'
-								class='absolute right-0 top-0 h-full bg-yellow-500 px-6 py-2 text-gray-900 rounded-full'
+								className='absolute right-0 top-0 h-full bg-yellow-500 px-6 py-2 text-gray-900 rounded-full'
 							>
-								<i class='fa fa-search' aria-hidden='true'></i>
+								<i
+									className='fa fa-search'
+									aria-hidden='true'
+								></i>
 							</button>
 						</div>
 					</div>
 				</div>
-
-				<div class=' items-center justify-around mx-4 hidden md:flex text-center'>
-					<button class='bg-yellow-500 px-6 py-2 rounded text-gray-900'>
-						Login
-					</button>
-					<span class='ml-4 text-white px-6 py-2 rounded bg-blue-900'>
+				<DropdownMenuDemo />
+				<div className='items-center justify-around mx-4 hidden md:flex text-center'>
+					<Button>
+						<Mail className='mr-2 h-4 w-4' /> Login with Email
+					</Button>
+					<span className='ml-4 text-black font-medium px-6 py-2 rounded bg-amber-500'>
 						Cart
-						<span class='ml-2 bg-yellow-500 px-2 py-1 rounded text-black font-bold'>
+						<span className='ml-2  py-1 rounded text-black font-medium'>
 							2
 						</span>
 					</span>
