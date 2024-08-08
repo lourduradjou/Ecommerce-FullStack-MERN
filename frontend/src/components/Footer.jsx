0fcadb9e93dee4e26d8d02fd4e0c99ce8d999b8a
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-08 14:56:58 +0530

    Add Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div className=' w-full flex justify-center items-end h-full'>
+			<footer className='py-1 bg-walmartBlue w-full'>
+				<p className='text-center text-black pb-6 pt-4 rounded-full'>
+					&copy; Walmart - 2023-2024, All Rights Reserved
+				</p>
+			</footer>
+		</div>
+	);
+}
+
+export default Footer;
